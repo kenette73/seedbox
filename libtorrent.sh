f3911ffc8564c48eb633d75711fe250f0691de63
kenette73 <kevin.ledoux578@orange.fr>
2016-12-28 20:59:35 +0100

    Create libtorrent.sh

---
+++ libtorrent.sh
@@ -0,0 +1,18 @@
+# Installation des pacquets pour Rtorrent et Libtorrent
+apt-get install -y git-core libtool libncurses5-dev libncursesw5-dev libcurl4-openssl-dev libcppunit-dev pkg-config
+
+echo "Paquets installés"
+
+# Installation et compilation de Libtorrent
+cd /tmp
+git clone https://github.com/rakshasa/libtorrent.git
+cd libtorrent
+./autogen.sh
+./configure --without-zlib
+make
+make install
+make clean
+make distclean
+rm -R /tmp/libtorrent
+
+echo "Libtorrent installée"
