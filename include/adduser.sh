71d4f5bdb7c02a8f5f883174624564d2b9891c42
kenette73 <kevin.ledoux578@orange.fr>
2016-12-31 17:59:40 +0100

    Update adduser.sh

--- include/adduser.sh
+++ include/adduser.sh
@@ -5,9 +5,12 @@
 read -p 'Choisissez un nom d utilisateur:' new_user
 adduser $new_user
 mkdir /home/$new_user/
+mkdir /home/$new_user/watch/
+mkdir /home/$new_user/torrent/
+mkdir /home/$new_user/sesion/
 chmod -R $new_user:$new_user /home/$new_user
 
 # Interdiction connection ssh
-Ajouter DenyUsers $new_user à /etc/ssh/sshd_config
-service ssh reload
+#Ajouter DenyUsers $new_user à /etc/ssh/sshd_config
+#service ssh reload
 
