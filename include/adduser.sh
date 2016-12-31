72f6539ac11cbb421f80df394ef1805170b34774
kenette73 <kevin.ledoux578@orange.fr>
2016-12-31 12:17:26 +0100

    Update adduser.sh

--- include/adduser.sh
+++ include/adduser.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
 # Création du nom d'utilisateur
 
-read -p 'Choisissez un nom d'utilisateur:' new_user
+read -p 'Choisissez un nom d utilisateur:' new_user
 adduser $new_user
 mkdir /home/$new_user/
 chmod -R $new_user:$new_user /home/$new_user
+
+# Interdiction connection ssh
+Ajouter DenyUsers $new_user à /etc/ssh/sshd_config
+service ssh reload
+
