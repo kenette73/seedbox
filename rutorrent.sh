2e20f3e809729bd915e7ffd53a83ebbeeac10b2b
kenette73 <kevin.ledoux578@orange.fr>
2017-01-01 13:40:40 +0100

    Delete rutorrent.sh

--- rutorrent.sh
+++
@@ -1,6 +0,0 @@
-# Installation de Rutorrent
-
-cd /var/www/html
-git clone https://github.com/Novik/ruTorrent
-chomd -R www-data:www-data /var/www/html/ruTorrent
-#rm -R /var/www/html/ruTorrent/plugins/*
