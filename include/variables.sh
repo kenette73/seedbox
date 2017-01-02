598ffe361864bb75c4121037aa1230e73681d4a9
kenette73 <kevin.ledoux578@orange.fr>
2017-01-02 13:29:29 +0100

    Update variables.sh

--- include/variables.sh
+++ include/variables.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # Variables definit par l'utilisateur
-read -p 'Choisissez un nom d utilisateur:' new_user
-grep -w "$new_user"
-read -p 'Choisissez un mot de passe:' new_passwd
 
 # Liste des variables
 
