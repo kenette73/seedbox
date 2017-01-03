0621221d60bd113a91668520c236f8db840abc1e
kenette73 <kevin.ledoux578@orange.fr>
2017-01-03 20:31:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,15 +33,10 @@ cp /tmp/seedbox/.rtorrent.rc /home/$NEW_USER/.rtorrent.rc
 chown $NEW_USER:$NEW_USER /home/$NEW_USER/.rtorrent.rc
 sed -i 's/@user/'$NEW_USER'/g' /home/$NEW_USER/.rtorrent.rc
 sed -i 's/@port/5000/g' /home/$NEW_USER/.rtorrent.rc
-
-#Installations des depots pour Fmpeg
-apt-get install -y software-properties-common
-
+sed -i 's/@rutorrent/'$RUTORRENT'/g' /home/$NEW_USER/.rtorrent.rc
 
 # Installations des paquets nécessaires à Rtorrent et Rutorrent
 apt-get install -y build-essential subversion autoconf automake curl gcc g++ rtorrent screen gzip mediainfo ffmpeg unrar zip
-
-echo "Paquets installés"
 
 # Configuration de Rtorrent
 cp /tmp/seedbox/rtorrent $RTORRENT
@@ -53,7 +48,7 @@ echo 'Rtorrent configuré'
 
 # Installation de Rutorrent
 cd $WWW
-git clone https://github.com/Novik/ruTorrent
+git clone https://github.com/Novik/ruTorrent rutorrent
 mkdir $CONF/$NEW_USER
 cp /tmp/seedbox/config/* $CONF/$NEW_USER/
 sed -i 's/@user/'$NEW_USER'/g' $CONF/$NEW_USER/config.php
