f3518565bf0cf9ea69304a1b5626fb269238e0ac
kenette73 <kevin.ledoux578@orange.fr>
2017-01-06 13:17:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@
 # Log Installation
 exec > >(tee "/tmp/seedbox/install.log") 2>&1
 
-#Installations des depots pour ffmpeg
+# Installations des depots pour ffmpeg
 apt-get install -y software-properties-common
 apt-add-repository "deb http://mirrors.online.net/debian jessie-backports main non-free contrib"
 
 # Vérifications des mise à jour
 sudo apt-get update
-echo "Serveur mis à jour"
 
 # Création de l'utilisateur et des répertoires
 read -p 'Choisissez un nom d utilisateur:' NEW_USER
@@ -19,8 +18,6 @@ mkdir -p /home/$NEW_USER/{watch,torrents,.session}
 chown -R $NEW_USER:$NEW_USER /home/$NEW_USER
 chmod -R 755 /home/$NEW_USER
 
-echo ' utilisateur crée'
-
 # Interdiction connection ssh
 #Ajouter DenyUsers $new_user à /etc/ssh/sshd_config
 #service ssh reload
@@ -28,7 +25,9 @@ echo ' utilisateur crée'
 # Include
 DIR="include"
 . "$DIR"/variables.sh
-. "$DIR"/apache.sh
+
+# Installations des paquets nécessaires à Rtorrent et Rutorrent
+apt-get install -y build-essential subversion autoconf automake curl gcc g++ rtorrent screen gzip mediainfo ffmpeg unrar zip apache2 apache2.2-common apache2-utils libapache2-mod-scgi libapache2-mod-php5
 
 # Création du fichier de configuration de rtorrent
 cp /tmp/seedbox/.rtorrent.rc /home/$NEW_USER/.rtorrent.rc
@@ -37,18 +36,32 @@ sed -i 's/@user/'$NEW_USER'/g' /home/$NEW_USER/.rtorrent.rc
 sed -i 's/@port/5000/g' /home/$NEW_USER/.rtorrent.rc
 sed -i 's/@rutorrent/'$RUTORRENT'/g' /home/$NEW_USER/.rtorrent.rc
 
-echo 'Rtorrent configuré'
-
-# Installations des paquets nécessaires à Rtorrent et Rutorrent
-apt-get install -y build-essential subversion autoconf automake curl gcc g++ rtorrent screen gzip mediainfo ffmpeg unrar zip
-
 # Configuration de Rtorrent
 cp /tmp/seedbox/rtorrent $RTORRENT
 chmod +x $RTORRENT
 update-rc.d rtorrent defaults 99
 sed -i 's/@user/'$NEW_USER'/g' $RTORRENT
 
-echo 'Rtorrent configuré'
+# Configuration de apache
+a2enmod ssl proxy_scgi auth_digest
+service apache2 force-reload
+
+# Création du certificat ssl
+mkdir /etc/apache2/ssl
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/apache2/ssl/rutorrent.pem -out /etc/apache2/ssl/rutorrent.pem
+chmod 600 /etc/apache2/ssl/rutorrent.pem
+
+# Accès Rutorrent
+cp /tmp/seedbox/config/rutorrent.conf /etc/apache2/sites-available/
+a2ensite rutorrent
+#rm /etc/apache2/ports.conf
+#cp /tmp/seedbox/config/ports.conf /etc/apache2/
+
+# Créations de l'utilisateur
+sudo htdigest -c /etc/apache2/.htpasswd rutorrent $NEW_USER
+
+# Redémarrage Apache2
+service apache2 restart
 
 # Installation de Rutorrent
 cd $WWW
@@ -60,7 +73,6 @@ sed -i 's/@user/'$NEW_USER'/g' $CONF/$NEW_USER/config.php
 sed -i 's/@port/5000/g' $CONF/$NEW_USER/config.php
 chown -R www-data:www-data $RUTORRENT
 chmod -R 755 $RUTORRENT
-#rm -R /var/www/html/ruTorrent/plugins/*
 
 #Démarrage de rtorrent
 service rtorrent start
