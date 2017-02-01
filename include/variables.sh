12cd0cd4a4e4ecf1f5c342aa150861459396d43e
kenette73 <kevin.ledoux578@orange.fr>
2017-02-01 21:51:45 +0100

    Update variables.sh

--- include/variables.sh
+++ include/variables.sh
@@ -3,7 +3,3 @@
 # Variables pour l'utilisateur
 
 # Variables répertoires
-RUTORRENT="/var/www/html/rutorrent"
-RTORRENT="/etc/init.d/rtorrent"
-WWW="/var/www/html"
-CONF="/var/www/html/rutorrent/conf/users"
